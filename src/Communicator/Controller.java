169bc75d9d7be82199854602513cb9a7bc511388
dongmingchao <1657883517@qq.com>
2017-07-18 16:18:54 +0800

    Studying

---
+++ src/Communicator/Controller.java
@@ -0,0 +1,8 @@
+package Communicator;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import Communicator.Main;
+public class Controller {
+    private Button send;
+    private Main app;
+}
